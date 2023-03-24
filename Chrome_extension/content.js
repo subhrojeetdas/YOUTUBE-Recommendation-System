953def590efd7778fd4128b3f40c991a2912eb6d
subhrojeetdas <subhrojeetdasofficial@gmail.com>
2023-03-24 20:58:47 +0530

    Added Button

--- Chrome_extension/content.js
+++ Chrome_extension/content.js
@@ -2,39 +2,58 @@
 if(document.body)
 {
   process();
+  fnDefineEvents();
+  //addText();
 }
 else 
 {
   document.addEventListener('DOMContentLoaded', process);
+  document.addEventListener('DOMContentLoaded', fnDefineEvents);
 }
+
 function process() 
 {
-  var button = document.createElement("button");
-  button.innerHTML = "My Button";
+  var btn = document.createElement("input");
+  btn.value = "Evaluate";
+  btn.type= "submit";
+  btn.id="eval_btn";
   str="results?search_query";
-  // Add an event listener to the button
-  if(window.location.toString().includes("results"))
+    
+  
+  //Add an event listener to the button
+  /*if(window.location.toString().includes("results"))
   {
     button.addEventListener("click", function() {
       alert("Show Positivity Percentage");
     });
-  }
+  }*/
   console.log("Hello");
   // Add the button to the page
-  var voicebtn = document.getElementById("search-form");
-  voicebtn.parentNode.insertBefore(button, voicebtn);
+  var voicebtn = document.getElementById("voice-search-button");
+  voicebtn.parentNode.appendChild(btn);
 }
-/*// Create the button element
-var button = document.createElement("button");
-button.innerHTML = "My Button";
 
-// Add an event listener to the button
-button.addEventListener("click", function() {
-  alert("Button was clicked!");
+function fnDefineEvents(){
+  // Add an event listener to the button
+
+document.getElementById("eval_btn").addEventListener("click", function(event) {
+  addText();
 });
+}
 
-// Add the button to the page
-var voicebtn = document.getElementById("voice-search-button");
-voicebtn.parentNode.insertAfter(button, voicebtn);*/
+function addText(){
+  var title=document.getElementsByClassName("yt-simple-endpoint style-scope ytd-video-renderer");
+  console.log(title.length);
+  for(let i=0;i<title.length/2;i++)
+  {
+    title[i*2].innerHTML=title[i*2].textContent+ " Hello"+ i.toString();
+  }
+  //title=title+" "+ "Hello";
+  //document.getElementById("video-title").innerHTML=title;
+  
+}
+
+
+
 
 
